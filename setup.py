2462bb367326e7e6067623114ba571e98c6565c8
lym <liyongming@weililiang1.com>
2025-04-15 11:33:38 +0800

    udpate for args

--- setup.py
+++ setup.py
@@ -1,29 +1,16 @@
-# Copyright (c) 2022 Xingchen Song (sxc19@tsinghua.org.cn)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import sys
 
 from setuptools import setup, find_packages
 
-version = sys.argv[-1].split('=')[1]
-sys.argv = sys.argv[0:len(sys.argv) - 1]
+# version = sys.argv[-1].split('=')[1]
+# sys.argv = sys.argv[0:len(sys.argv) - 1]
 
 with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setup(
     name="WeTextProcessing-Fix-2",
-    version=version,
+    version="0.0.1",
     author="Lyming",
     author_email="331661029@qq.com",
     long_description=long_description,
